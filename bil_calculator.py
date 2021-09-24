4eb317f48581233d8c3c1ddd787e8e3f500e0837
Dilbara Asanalieva <dilbara2025@gmail.com>
2021-09-24 14:28:10 +0600

    calculate bill with tip

--- bil_calculator.py
+++ bil_calculator.py
@@ -7,4 +7,5 @@ total_tip_amount = total * tip_as_percent
 total_bill = total + total_tip_amount
 bill_per_person = total_bill / people
 final_amont = round(bill_per_person, 2)
+#Result
 print(f"Each person should pay: ${final_amont}")
