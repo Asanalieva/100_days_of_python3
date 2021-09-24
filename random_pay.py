9f3ca1b58e412a24e17cd3529a6f526f85393943
Dilbara Asanalieva <dilbara2025@gmail.com>
2021-09-24 14:19:45 +0600

    who will pay bill

--- random_pay.py
+++ random_pay.py
@@ -2,7 +2,7 @@
 import random
 names_string = input("Give me everybody's names, separated by a comma: ")
 names = names_string.split(", ")
-
+#Result
 person = random.choice(names)
 print(person, "is going to buy the meal today!")
 
