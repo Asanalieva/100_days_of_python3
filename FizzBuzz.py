70c77cc46fb4c99b36e44c878764985693c5741d
Dilbara Asanalieva <dilbara2025@gmail.com>
2021-09-27 16:25:40 +0600

    FizzBuzz game

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,17 @@
-for i in range(1,101):
-  if i % 3 == 0 and i % 5 == 0:
-    print("FizzBuzz")
-  elif i % 3 == 0:
-    print("Fizz")
-  elif i % 5 == 0:
-    print("Buzz")
-  else:
-    print(i)   
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+''' program will print each number from 1 to 100 in turn.` 
+
+>   `When the number is divisible by 3 then instead of printing the number it will print "Fizz".` 
+
+>     `When the number is divisible by 5, then instead of printing the number it will print "Buzz".` 
+
+>       `And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it will print "FizzBuzz" '''
